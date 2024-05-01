ad5867bfbeb5a355149988f562f91a920fdedacf
Gopal Kaul <gopalkaul5@gmail.com>
2024-05-02 00:39:57 +0530

    Create other types

---
+++ app/(types)/CurrencyFuturesItem.d.ts
@@ -0,0 +1,12 @@
+export type CurrencyFuturesItem = {
+  id: string;
+  dispName: string;
+  excToken: number;
+  lot: number;
+  tick: number;
+  asset: string;
+  freezeQty: number;
+  weekly: string;
+  undId: string;
+  lotMulti: number;
+};
