b9b27a8819ad82ba0e8988fee35fe13f02037e15
Gopal Kaul <gopalkaul5@gmail.com>
2024-05-02 00:40:21 +0530

    Make it work :3

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,58 @@
 import SearchComponent from "./(components)/SearchComponent";
 import TypeSelectorComponent from "./(components)/TypeSelector";
 import { CommodityFuturesItem } from "./(types)/CommodityFuturesItem";
+import { CurrencyFuturesItem } from "./(types)/CurrencyFuturesItem";
+import { FutureContractsItem } from "./(types)/FutureContractsItem";
+import { NSEOptionsItem } from "./(types)/NSEOptionsItem";
 
 export default async function Home({ searchParams }) {
   const {
     type,
     name,
-  }: { type: "commodityfutures" | undefined; name: string | undefined } =
-    searchParams;
+  }: {
+    type:
+      | "futurecontracts"
+      | "commodityfutures"
+      | "currencyfutures"
+      | "nseoptions"
+      | undefined;
+    name: string | undefined;
+  } = searchParams;
   console.log(searchParams);
-  const commodityFutures: CommodityFuturesItem[] = (
-    await (await fetch(process.env.URL + "/apis/commodityfutures")).json()
-  ).data;
+  let data:
+    | CommodityFuturesItem[]
+    | CurrencyFuturesItem[]
+    | FutureContractsItem[]
+    | NSEOptionsItem[] = [];
+  if (type !== undefined)
+    if (type === "nseoptions") {
+      data = [
+        ...(
+          await (
+            await fetch(process.env.URL + "/apis/" + type + "/ce", {
+              cache: "no-store",
+            })
+          ).json()
+        ).data,
+        ...(
+          await (
+            await fetch(process.env.URL + "/apis/" + type + "/pe", {
+              cache: "no-store",
+            })
+          ).json()
+        ).data,
+      ];
+    } else
+      data = (await (await fetch(process.env.URL + "/apis/" + type)).json())
+        .data;
   return (
     <main>
       <TypeSelectorComponent />
       <SearchComponent />
       {type !== undefined && (
         <ul>
-          {(type === "commodityfutures" ? commodityFutures : commodityFutures)
+          {data
+            .slice(0, 50)
             .filter((x) =>
               x.dispName.toLowerCase().includes(name?.toLowerCase()),
             )
