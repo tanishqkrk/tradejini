a0bea72678e31f9826bcd46807809aebbccade29
Gopal Kaul <gopalkaul5@gmail.com>
2024-05-03 09:42:49 +0530

    Addd loading

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="w-screen h-screen bg-black text-white font-bold text-5xl grid place-items-center">
+      Loading!
+    </div>
+  );
+}
