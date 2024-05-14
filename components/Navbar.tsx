8495299d3bc14827d705d76dbfe13e7d7482a352
Gopal Kaul <gopalkaul5@gmail.com>
2024-05-14 23:56:08 +0530

    Work on navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className=" py-4 p-3">
+      <div className="relative w-full h-full p-[1px] rounded-2xl">
+        <div className="flex justify-between items-center px-10 rounded-2xl py-6">
+          <img src="/logo.svg" className="w-36" alt="" />
+          <div className="rounded-full bg-[#19ad63] py-1 px-0.5 flex flex-row gap-x-4">
+            <Link
+              className="rounded-full bg-white text-black px-5 py-3 shadow-2xl"
+              href={"#"}
+            >
+              Home
+            </Link>
+            <Link className="px-8 py-3 text-white rounded-full" href="#">
+              About
+            </Link>
+            <Link className="px-8 py-3 text-white rounded-full" href="#">
+              Pricing
+            </Link>
+            <Link className="px-8 py-3 text-white rounded-full" href="#">
+              Jinversity
+            </Link>
+            <Link className="px-8 py-3 text-white rounded-full" href="#">
+              Media
+            </Link>
+            <Link className="px-8 py-3 text-white rounded-full" href="#">
+              Support
+            </Link>
+          </div>
+          <div className="flex flex-row gap-x-4 items-center">
+            <Link
+              href="#"
+              className="border-[#19ad63] border-2 text-[#19ad63] font-bold px-5 py-2 rounded-xl"
+            >
+              Login
+            </Link>
+            <Link
+              href="#"
+              className="border-[#19ad63] border-2 bg-[#19ad63] text-white font-bold px-5 py-2 rounded-xl"
+            >
+              Signup
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
